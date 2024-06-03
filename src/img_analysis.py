731086a7cbe5d0bdad4e3e5078e436841b877e96
林祐德 <qwerty239qwe@gmail.com>
2024-06-03 20:55:26 +0800

    add functions for searching hparams

--- src/img_analysis.py
+++ src/img_analysis.py
@@ -14,11 +14,12 @@ from skimage.exposure import adjust_sigmoid
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from src.utils import create_folder_for_each_czi, split_czi_to_tiffs, list_tif_in_dir
+from src.utils import create_folder_for_each_czi, split_czi_to_tiffs, list_tif_in_dir, HyperParam, get_hparams_grid, SearchDomain
 from src.skeleton import SkeletonAnalyzer
 from src.masks import get_binary_tmrm, get_binary_nucleus, get_binary_img
 
 from tqdm import tqdm
+import json
 
 
 DEFAULT_FIELDS = ['Img ID', 
@@ -33,6 +34,8 @@ DEFAULT_FIELDS = ['Img ID',
                   'Branch Length per Cell',
                   'Average Node Degree',
                   'Average Membrane Potential']
+
+DEFAULT_HPARAMS_DIR_PATH = Path(__file__).parent.parent / "hparams"
 
 
 def single_cell_mito_analysis(input_dir_path, 
@@ -76,8 +79,21 @@ def single_cell_mito_analysis(input_dir_path,
     return df
 
 
-def population_mito_analysis(input_dir_path, path_dic, sel_fields):
+def population_mito_analysis(input_dir_path, 
+                             hparam_dir_path,
+                             path_dic, 
+                             sel_fields):
     result_dic = {}
+    hparam_dir_path = Path(hparam_dir_path)
+    tmrm_params, nuc_params = {}, {}
+    if (hparam_dir_path / "tmrm.json").is_file():
+        with open(hparam_dir_path / "tmrm.json", "r+") as f:
+            tmrm_params = json.load(f)
+
+    if (hparam_dir_path / "nucleus.json").is_file():
+        with open(hparam_dir_path / "nucleus.json", "r+") as f:
+            nuc_params = json.load(f)
+
     for i, (img_id, folder_path) in tqdm(enumerate(path_dic.items()), total=len(path_dic)):
         tmrm_path = folder_path  / f"{folder_path.stem}0000.tif"
         nucleus_path = folder_path  / f"{folder_path.stem}0001.tif"
@@ -90,8 +106,12 @@ def population_mito_analysis(input_dir_path, path_dic, sel_fields):
             tqdm.write(f"{mask_path} exists. Skipped mask calculation")
             binary_tmrm = cv2.imread(str(mask_path.resolve()),0)
         else:
-            binary_tmrm = get_binary_tmrm(tmrm_img, mask_img_path=mask_path)
-        binary_nucleus = get_binary_nucleus(nucleus_img, mask_img_path=nucleus_mask_path)
+            binary_tmrm = get_binary_tmrm(tmrm_img, mask_img_path=mask_path, **tmrm_params)
+        if nucleus_mask_path.is_file():
+            tqdm.write(f"{nucleus_mask_path} exists. Skipped mask calculation")
+            binary_nucleus = cv2.imread(str(nucleus_mask_path.resolve()),0)
+        else:
+            binary_nucleus = get_binary_nucleus(nucleus_img, mask_img_path=nucleus_mask_path, **nuc_params)
         skeleton_analyzer = SkeletonAnalyzer(binary_tmrm, tmrm_img, binary_nucleus, 
                                              default_pixel_size=1, img_id=img_id)
         result_dic[i] = skeleton_analyzer.get_results(sel_fields)
@@ -102,20 +122,108 @@ def population_mito_analysis(input_dir_path, path_dic, sel_fields):
     return df
 
 
+def _prepare_hparams(n, 
+                     r=None, 
+                     use_tmrm=True,
+                     hp_to_test="default",
+                     seed=42,
+                     step_like=True):
+    hp_adjust_sigmoid__added_cutoff = HyperParam("added_cutoff", [1e-6, 0.1], search_domain=SearchDomain.loguniform)
+    hp_adjust_sigmoid__gain = HyperParam("gain", [2, 11], search_domain=SearchDomain.intuniform)
+    hp_denoise_tv_chambolle__weight = HyperParam("weight", [1e-2, 1e2], search_domain=SearchDomain.loguniform)
+    hp_median_filter__filter_size = HyperParam("filter_size", [2, 7], search_domain=SearchDomain.intuniform)
+    hp_dilate__dilate_kernel_size = HyperParam("dilate_kernel_size", [2, 7], search_domain=SearchDomain.intuniform)
+    
+    if use_tmrm:
+        hp_to_test = [hp_adjust_sigmoid__added_cutoff,
+                    hp_adjust_sigmoid__gain,
+                    hp_denoise_tv_chambolle__weight] if hp_to_test == "default" else hp_to_test
+    else:
+        hp_to_test = [hp_adjust_sigmoid__added_cutoff,
+                    hp_adjust_sigmoid__gain,
+                    hp_median_filter__filter_size] if hp_to_test == "default" else hp_to_test
+    
+    return get_hparams_grid(n=n, random_k=r, seed=seed, step_like=step_like, *hp_to_test)
+    
+
+def search_mask(path_dic, 
+                n, 
+                r=None, 
+                use_tmrm=True,
+                hp_to_test="default",
+                seed=42,
+                step_like=True):
+    hparam_grid = _prepare_hparams(n=n,
+                                   r=r,
+                                   use_tmrm=use_tmrm,
+                                   hp_to_test=hp_to_test,
+                                   seed=seed,
+                                   step_like=step_like)
+    
+    pbar = tqdm(total=len(hparam_grid) * len(path_dic))
+    for i, (img_id, folder_path) in enumerate(path_dic.items()):
+        img_path = folder_path  / (f"{folder_path.stem}0000.tif" if use_tmrm else f"{folder_path.stem}0001.tif")
+        mask_folder_path: Path = folder_path / ("tmrm_masks" if use_tmrm else "nucleus_masks")
+        mask_folder_path.mkdir(exist_ok=True, parents=True)
+        
+        raw_img = cv2.imread(str(img_path.resolve()),0)
+
+        df = pd.DataFrame(list(hparam_grid))
+        df["is_best"] = "X"  # let users decide
+        df.index = df.index.to_series().apply(lambda x: f"trial_{x}")
+        df.to_csv(mask_folder_path / "hparam_tested.csv")
+
+        for i, hp in enumerate(hparam_grid):
+            fn = [f"{k}_{v:.2E}" for k, v in hp.items()]
+            fn = "__".join(fn)
+            if use_tmrm:
+                get_binary_tmrm(tmrm=raw_img,
+                                mask_img_path=mask_folder_path / f"trial_{i}.png",
+                                **hp)
+            else:
+                get_binary_nucleus(nucleus=raw_img,
+                                   mask_img_path=mask_folder_path / f"trial_{i}.png",
+                                   **hp)
+            pbar.update(1)
+
+
+def sel_best_param(file_path, save_param_path, is_tmrm):
+    df = pd.read_csv(file_path, index_col=0)
+    best_params = df[df["is_best"] != "X"].drop(columns=["is_best"]).iloc[0, :].to_dict()
+    save_param_path = Path(save_param_path)
+    save_param_path.mkdir(exist_ok=True, parents=True)
+
+    with open(save_param_path / ("tmrm.json" if is_tmrm else "nucleus.json"), "w+") as f:
+        json.dump(best_params, f)
+
+
 def load_args():
     arg_parser = ArgumentParser()
     arg_parser.add_argument("-i", "--input_dir",
-                            help="Input folder path")
+                            help="Input folder/file path")
+    arg_parser.add_argument("-o", "--output",
+                            help="Output param file's folder")
+    arg_parser.add_argument("-b", "--best_params", default=DEFAULT_HPARAMS_DIR_PATH,
+                            help="Path to the folder with best parameters (.json) for performing binarization."
+                                 "Default to the default parameter files")
     arg_parser.add_argument("-m", "--method",
                             choices=["population",
                                      "sc",
-                                     "search_mask"],
-                            help="Used method name, choices are search_mask, population, and sc")
+                                     "search_mask",
+                                     "sel_best_param"],
+                            help="Used method name, choices are search_mask, sel_best_param, population, and sc")
     arg_parser.add_argument("-e", "--exp", nargs="+", default=None,
                             help="")
     arg_parser.add_argument("-d", "--dish", nargs="+", default=None)
     arg_parser.add_argument("-f", "--frame", nargs="+", default=None)
     arg_parser.add_argument("-y", "--dye", nargs="+", default=["mitotracker"])
+    arg_parser.add_argument("-t", "--target_czi", nargs="+", help="The czi files to be tested. Only used in search_mask method")
+    arg_parser.add_argument("-n", "--n_sample", type=int, default=3, 
+                            help="Size of each hparameter in the hyperparameter pool. The script will test n^(hp number) samples if -r is not specified")
+    arg_parser.add_argument("-r", "--random_pick", type=int, default=10,
+                            help="Number of tested hparameters. The script will test n^(hp number) samples if -r is not specified")
+    arg_parser.add_argument("-u", "--use_nucleus", action="store_true",
+                            help="To use nucleus as mask search target or not (if not, use TMRM images.)")
     arg_parser.add_argument("-s", "--sep_dir", action="store_true",
                             help="To save masks in separated folders for each czi file or not")
     return arg_parser.parse_args()
@@ -126,6 +234,7 @@ if __name__ == "__main__":
     if args.method.lower() == "population":
         path_dic=split_czi_to_tiffs(args.input_dir)
         population_mito_analysis(args.input_dir,
+                                 hparam_dir_path=args.best_params,
                                  path_dic=path_dic, 
                                  sel_fields=DEFAULT_FIELDS)
     elif args.method.lower() == "sc":
@@ -134,3 +243,10 @@ if __name__ == "__main__":
         else:
             sel_levels = [[f"{exp} {dish}-{frame}" for exp, dish, frame in product(args.exp, args.dish, args.frame)], args.dye]
         single_cell_mito_analysis(args.input_dir, sel_levels)
+
+    elif args.method.lower() == "search_mask":
+        path_dic=split_czi_to_tiffs(args.input_dir, target_czi=args.target_czi,)
+        search_mask(path_dic=path_dic, n=args.n_sample, r=args.random_pick, use_tmrm=not args.use_nucleus)
+
+    elif args.method.lower() == "sel_best_param":
+        sel_best_param(args.input_dir, args.output, not args.use_nucleus)
--- src/utils.py
+++ src/utils.py
@@ -2,13 +2,20 @@ from typing import Union, List
 from pathlib import Path
 from itertools import product
 from aicsimageio import AICSImage
+from collections import namedtuple
+from enum import Enum
 import tifffile
+import os
+import numpy as np
 
 
-def create_folder_for_each_czi(folder_path: Union[str, Path], dest_folder=None) -> dict:
+def create_folder_for_each_czi(folder_path: Union[str, Path], dest_folder=None, target_czi=None) -> dict:
     dest_folder = dest_folder if dest_folder is not None else folder_path
     path_dic = {fn.stem: Path(dest_folder, fn.stem) 
                 for fn in Path(folder_path).iterdir() if fn.suffix == ".czi"}
+    if target_czi is not None:
+        path_dic = {k: v for k, v in path_dic.items() if k in target_czi}
+
     for name, path in path_dic.items():
         Path(path).mkdir(parents=True, exist_ok=True)
     return path_dic
@@ -29,10 +36,14 @@ def sep_czi_channels_to_tiff(czi_file_path, save_dir, id_format="{czi_name}{i:04
         )
 
 
-def split_czi_to_tiffs(folder_path, dest_folder=None, sep_dir=True, id_format="{czi_name}{i:04}", axes="TCZYX"):
-    all_czi_paths = [fn for fn in Path(folder_path).glob("*.czi")]
+def split_czi_to_tiffs(folder_path, target_czi=None, dest_folder=None, sep_dir=True, id_format="{czi_name}{i:04}", axes="TCZYX"):
+    if target_czi is None:
+        all_czi_paths = [fn for fn in Path(folder_path).glob("*.czi")]
+    else:
+        all_czi_paths = [fn for fn in Path(folder_path).glob("*.czi") if fn.stem in target_czi]
     if sep_dir:
-        dest_folders = create_folder_for_each_czi(folder_path=folder_path, dest_folder=dest_folder)
+        dest_folders = create_folder_for_each_czi(folder_path=folder_path, dest_folder=dest_folder,
+                                                  target_czi=target_czi)
         for czi_path in all_czi_paths:
             sep_czi_channels_to_tiff(czi_path, dest_folders[czi_path.stem], id_format=id_format, axes=axes)
         return dest_folders
@@ -44,6 +55,10 @@ def split_czi_to_tiffs(folder_path, dest_folder=None, sep_dir=True, id_format="{
 
 
 def list_all_folders(rootpath: str) -> List[Path]:
+    """
+    A deprecated function.
+    Get all paths created for all the well names
+    """ 
     well_list = ['A','B','C','D','E','F','G','H']
     folder_list = [Path(rootpath, f"{w}{j}") for w in well_list for j in range(1, 13)]
     return folder_list
@@ -51,6 +66,7 @@ def list_all_folders(rootpath: str) -> List[Path]:
 
 def list_dir_tif(path, list_to_read, list_to_save,list_of_name):
     """
+    A deprecated function.
     Get all the tif file in a folder and return the file paths to save and the file stems
     """ 
     for file in os.listdir(path): 
@@ -75,3 +91,49 @@ def list_tif_in_dir(folder_path: Union[str, Path],
     for vdir in valid_dirs:
         file_names.extend(list(vdir.rglob("*.tif")))
     return file_names
+
+
+class SearchDomain(Enum):
+    uniform = "u"
+    loguniform = "lu"
+    categorical = "c"
+    intuniform = "iu"
+
+
+HyperParam = namedtuple("HyperParam",
+                        ["name", "search_range", "search_domain"],)
+
+
+def get_hparams_grid(*hparams, n, random_k=None, seed=42, step_like=True, **kwargs):
+    assert [isinstance(hp, HyperParam) for hp in hparams], "Passed hparams must be HyperParams"
+    rng = np.random.default_rng(seed=seed)
+    hparam_choices = []
+    hparam_order = []
+    
+    for hparam in hparams:
+        hparam_order.append(hparam.name)
+        low, high = hparam.search_range if hparam.search_domain != SearchDomain.categorical else (0, len(hparam.search_range))
+            
+        if hparam.search_domain == SearchDomain.categorical:
+            choices = rng.choice(hparam.search_range, replace=False, size=(min(n, len(hparam.search_range)),))
+        elif hparam.search_domain == SearchDomain.loguniform:
+            choices = np.exp(rng.uniform(low=np.log(low), 
+                                         high=np.log(high), 
+                                         size=(n,))) if not step_like else np.logspace(np.log10(low), np.log10(high), n,
+                                                                                       base=10)
+        elif hparam.search_domain == SearchDomain.uniform:
+            choices = rng.uniform(low=low, 
+                                  high=high, size=(n,)) if not step_like else np.linspace(low, high, n)
+        elif hparam.search_domain == SearchDomain.intuniform:
+            step = (high - low) / (n - 1)
+            choices = rng.integers(low=hparam.search_range[0], high=hparam.search_range[1], size=(n,)) \
+                      if not step_like else np.arange(low, high+1, step).astype(int)
+        hparam_choices.append(choices)
+    hparam_sets = product(*hparam_choices)
+    hparam_sets = [dict(zip(hparam_order, hset)) for hset in hparam_sets]
+    if random_k is None:
+        return hparam_sets
+    
+    assert random_k < np.power(n, len(hparams))
+    
+    return rng.choice(hparam_sets, size=(random_k,), replace=False)
